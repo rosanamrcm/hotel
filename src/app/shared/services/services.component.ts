07960b6e4a5f3d36bb5975faca638f719e5a1f85
rosanamrcm <mrcm1975@gmail.com>
2026-01-29 21:01:31 -0300

    responsividade/ajustes home page

--- src/app/shared/services/services.component.ts
+++ src/app/shared/services/services.component.ts
@@ -9,4 +9,49 @@ import { Component } from '@angular/core';
 })
 export class ServicesComponent {
 
+  services = [
+    {
+      icon: 'icons/restaurant.svg',
+      title: 'Restaurant',
+      description: 'Lorem ipsum dolor sit amet, consectetur'
+    },
+    {
+      icon: 'icons/transport.svg',
+      title: 'Transport Facility',
+      description: 'Lorem ipsum dolor sit amet, consectetur'
+    },
+    {
+      icon: 'icons/spa.svg',
+      title: 'Spa & Salon',
+      description: 'Lorem ipsum dolor sit amet, consectetur'
+    },
+    {
+      icon: 'icons/wifi.svg',
+      title: 'Free Wifi',
+      description: 'Lorem ipsum dolor sit amet, consectetur'
+    },
+    {
+      icon: 'icons/swimming-pool.svg',
+      title: 'Swimming Pool',
+      description: 'Lorem ipsum dolor sit amet, consectetur'
+    },
+    {
+      icon: 'icons/mini-bar.svg',
+      title: 'Mini Bar',
+      description: 'Lorem ipsum dolor sit amet, consectetur'
+    },
+    {
+      icon: 'icons/conference-room.svg',
+      title: 'Conference Room',
+      description: 'Lorem ipsum dolor sit amet, consectetur'
+    },
+    {
+      icon: 'icons/game-room.svg',
+      title: 'Game Room',
+      description: 'Lorem ipsum dolor sit amet, consectetur'
+    }
+  ];
 }
+
+
+
